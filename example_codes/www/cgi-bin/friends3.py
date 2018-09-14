0e36df9e416b2f012b8bc289c83a489f0263a879
pytoday <jackietsui72@gmail.com>
2018-09-14 20:18:43 +0800

    create file

---
+++ example_codes/www/cgi-bin/friends3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# title          : friends3.py
+# description    :
+# author         : JackieTsui
+# organization   : pytoday.org
+# date           : 2018/9/14 20:01
+# email          : jackietsui72@gmail.com
+# notes          :
+# ==================================================
+
+# Import the module needed to run the script
