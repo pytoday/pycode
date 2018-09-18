450c7c0f98b4ff62e27d59224210d689708ad48b
pytoday <jackietsui72@gmail.com>
2018-09-18 21:35:35 +0800

    cgi cookies

--- example_codes/www/cgi-bin/cookies_cgi.py
+++ example_codes/www/cgi-bin/cookies_cgi.py
@@ -79,18 +79,96 @@ class AdvCgi:
             usercook = cookstatus = self.cookies['user']
         print('%s%s' % (AdvCgi.header, AdvCgi.formhtml % (AdvCgi.url, cookstatus, usercook, self.who, ''.join(langstr), self.fn)))
 
-        errhtml = '''
-        <html><head><title>Cookies CGI Demo</title></head>
-        <body>
-        <h3>ERROR</h>
-        <b>%s</b><p>
-        <form><input type=button value=back onclick="window.history.back()></form>
-        </body>
-        </html>
-        '''
+    errhtml = '''
+    <html><head><title>Cookies CGI Demo</title></head>
+    <body>
+    <h3>ERROR</h>
+    <b>%s</b><p>
+    <form><input type=button value=back onclick="window.history.back()></form>
+    </body>
+    </html>
+    '''
+
+    def showerror(self):
+        print(AdvCgi.header + AdvCgi.errhtml % self.error)
+
+    reshtml = '''
+    <html>
+    <head><title>Cookie CGI Demo</title></head>
+    <body>
+   <h2>Your upload data</h2>
+   <h3>your name is <b>%s</b></h3> 
+   <h3>your can program in the follow lang:</h3>
+   <ul>%s</ul>
+   <h3>you uploaded file...<br> Name: <i>%s</i><br>Contents:</h3>
+   <pre>%s</pre>
+   Click<a href="%s"><b>here</b></a>
+    </body>
+    </html>
+    '''
+
+    def setcppcookies(self):
+        for eachcook in self.cookies.keys():
+            print('Set-Cookie: CPP%s=%s; path=/' % (eachcook, quote(self.cookies[eachcook])))
+
+    def doresult(self):
+        maxbyte = 4096
+        langlist = ''.join('<li>%s<br>' % eachlang for eachlang in self.langs)
+        filedata = self.fp.read(maxbyte)
+        if len(filedata) == maxbyte:
+            filedata = '%s%s' % (filedata, '...<b><i>(file truncated due to size)</i></b>')
+            self.fp.close()
+        if filedata == '':
+            filedata = '<b><i>(file not given or upload error)</i></b>'
+        filename = self.fn
+
+        if not ('user' in self.cookies and self.cookies['user']):
+            cookstatus = '<i>(cookie has not been set yet)</i>'
+            usercook = ''
+        else:
+            usercook = cookstatus = self.cookies['user']
+
+        self.cookies['info'] = ':'.join((self.who, ','.join(self.langs, ','), filename))
+        self.setcppcookies()
+        print('%s%s' % (AdvCgi.header, AdvCgi.reshtml % (cookstatus, self.who, langlist, filename, filedata, AdvCgi.url)))
 
     def go(self):
-        pass
+        self.cookies = {}
+        self.error = []
+        form = FieldStorage()
+        if not form.keys():
+            self.showform()
+            return
+
+        if 'person' in form:
+            self.who = form['person'].value.strip().title()
+            if self.who == '':
+                self.error = 'your name is required.(blank)'
+            else:
+                self.error = 'your name is required.(missing)'
+        self.cookies['user'] = unquote(form['cookies'].value.strip()) if 'cookie' in form else ''
+        if 'lang' in form:
+            langdata = form['lang']
+            if isinstance(langdata, list):
+                self.langs = [eachlang.value for eachlang in langdata]
+            else:
+                self.langs = [langdata.value]
+        else:
+            self.error = 'At lease one language required.'
+        if 'upfile' in form:
+            upfile = form['upfile']
+            self.fn = upfile.filename or ''
+            if upfile.file:
+                self.fp = upfile.file
+            else:
+                self.fp = StringIO('(no data)')
+        else:
+            self.fp = StringIO('(no data)')
+            self.fn = ''
+        if not self.error:
+            self.doresult()
+        else:
+            self.showerror()
 
 
 if __name__ == '__main__':
