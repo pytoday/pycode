e4f2cee9455ee51912787229156ca9a8d6714ed7
pytoday <jackietsui72@gmail.com>
2019-04-02 21:20:41 +0800

    add vector.py

---
+++ FluentPython/pythonic/vector.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# title          :vector.py
+# description    :
+# author         :JackieTsui
+# organization   :pytoday.org
+# date           :2019/4/2 21:19
+# email          :jackietsui72@gmail.com
+# notes          :
+# ==================================================
+
+# Import the module needed to run the script
