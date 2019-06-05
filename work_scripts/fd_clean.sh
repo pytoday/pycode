a4daaec49fe7d7127db15038356aa82692b629e3
pytoday <jackietsui72@gmail.com>
2019-06-05 17:38:11 +0800

    change temp file to share memory

--- work_scripts/fd_clean.sh
+++ work_scripts/fd_clean.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-lsof -n | grep delete |grep "/data" | sort -nrk 7 | head -n 100 | awk '{print $2}' > /root/pid.tmp
-for i in $(cat /root/pid.tmp)
+TMP_PID=/dev/shm/pid.tmp
+FD=/dev/shm/fd.tmp
+lsof -n | grep delete |grep "/data" | sort -nrk 7 | head -n 100 | awk '{print $2}' > "${TMP_PID}"
+for i in $(cat ${TMP_PID})
 do
-	cd /proc/"$i"/fd && ls -lh | grep '/data/.*log\..*[0-9] (deleted)' | awk '{print $9}' > /root/fd.tmp
-	for j in $(cat /root/fd.tmp)
+	cd /proc/"$i"/fd && ls -lh | grep '/data/.*log\..*[0-9] (deleted)' | awk '{print $9}' > "$FD"
+	for j in $(cat $FD)
 	do
 	cd /proc/"$i"/fd && echo > "$j"
 	done
