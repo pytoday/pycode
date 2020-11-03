2c25a52b81e23918eabac295224d7b89fcbc1603
pytoday <jackietsui72@gmail.com>
2020-11-03 18:30:28 +0800

    on_delete

--- StuDjango/stuapp/models.py
+++ StuDjango/stuapp/models.py
@@ -8,7 +8,7 @@ class Topic(models.Model):
     """Study title"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User,on_delete=None)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
@@ -16,7 +16,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Studied details about something."""
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
